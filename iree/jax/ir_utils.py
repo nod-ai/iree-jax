1665a9d4b3851447b30a135d06b16ac79deaef88
Boian Petkantchin <boian@nod-labs.com>
2023-01-09 16:28:51 -0800

    Revert fix for "type" argument of ml_program_d.GlobalOp.__init__

--- iree/jax/ir_utils.py
+++ iree/jax/ir_utils.py
@@ -48,7 +48,9 @@ def create_global(symbol_table: ir.SymbolTable,
   op = ml_program_d.GlobalOp(
       sym_visibility=ir.StringAttr.get(visibility),
       sym_name=ir.StringAttr.get(symbol),
-      type_=ir.TypeAttr.get(ir_type),
+      # On one occasion Jax generated the init function with
+      # argument named "type_" instead of just "type".
+      type=ir.TypeAttr.get(ir_type),
       is_mutable=ir.UnitAttr.get() if mutable else None,
       value=initial_value,
   )
